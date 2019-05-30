52af1de44b0d73d86e7f6ad9e311bd96c83afbd5
jie-d-cheng <jie.d.cheng@outlook.com>
2019-05-31 01:06:10 +0800

    location arguement sample

---
+++ ping05.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ $# -eq 0 ];then
+	echo "usage: `basename $0` file "
+	exit
+fi
+
+if [ ! -f $1 ]; then
+	echo "error file!"
+	exit
+fi
+
+for ip in `cat $1`
+do
+	ping -c1 $ip &>/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$ip is up."
+	else
+		echo "$ip is down"
+	fi
+done
