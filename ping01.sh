faba13762fd689b3f40eb9f47c27e299c196d9e7
jie-d-cheng <jie.d.cheng@outlook.com>
2019-05-31 01:13:11 +0800

    bash代码中混入python3的编程

--- ping01.sh
+++ ping01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ping -c1 10.20.3.1 &>/dev/null && echo "10.20.3.1 is up" ||echo "10.20.3.1 is down"
 
-# 如果EOF开始的时候前面没有 -， 那么EOF结束时前面就不能有tab缩进,但是有-，依然不能有空格
+# 如果EOF开始的时候前面没有 -， 那么EOF结束时前面就不能有tab缩进, 但是有-，依然不能有空格.
 /usr/bin/python3 <<-EOF
 print("Hello World From python3...")
 EOF
